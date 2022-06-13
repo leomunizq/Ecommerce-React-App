a9c2cf2ec1cd9282c9069f1df2ace439d249387f
leomunizq <leonardo.mdq@hotmail.com>
2022-06-13 18:58:53 +0200

    Navbar StyledComponent.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Search } from '@material-ui/icons'
+import { Search, ShoppingCartOutlined } from '@material-ui/icons'
+import { Badge } from '@material-ui/core'
 
 const Container = styled.div`
   height: 60px;
@@ -9,6 +10,7 @@ const Container = styled.div`
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
+  align-items: center;
   justify-content: space-between;
 `
 
@@ -27,12 +29,31 @@ const SearchContainer = styled.div`
   border: 1px solid lightgray;
   display: flex;
   align-items: center;
+  margin-left: 25px;
+  padding: 5px;
 `
+const Input = styled.input`
+  border: none;
+`
+
 const Center = styled.div`
   flex: 1;
+  text-align: center;
 `
+const Logo = styled.h1`
+  font-weight: bold;
+`
+
 const Right = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 20px;
 `
 
 const Navbar = () => {
@@ -42,12 +63,22 @@ const Navbar = () => {
         <Left>
           <Languages>EN</Languages>
           <SearchContainer>
-            input
-            <Search />
+            <Input />
+            <Search style={{ color: 'gray', fontSize: '16px' }} />
           </SearchContainer>
         </Left>
-        <Center>Center</Center>
-        <Right>Right</Right>
+        <Center>
+          <Logo>LOGO.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>Register</MenuItem>
+          <MenuItem>Sign In</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   )
