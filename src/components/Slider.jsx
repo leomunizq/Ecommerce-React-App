07d196e130ee7a8d543ab80814d3bcc45c953223
leomunizq <leonardo.mdq@hotmail.com>
2022-06-13 19:50:22 +0200

    Slider srrows.

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,21 +3,37 @@ import styled from 'styled-components'
 
 const Container = styled.div`
   width: 100%;
-  height: 100hv;
+  height: 100vh;
   display: flex;
+  background-color: coral;
+  position: relative;
 `
-const ArrowLeftOutlined = styled.div`
+const Arrow = styled.div`
   width: 50px;
   height: 50px;
   background-color: #fff7f7;
   border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${props => props.direction === 'left' && '10px'};
+  right: ${props => props.direction === 'right' && '10px'};
+  cursor: pointer;
+  margin: auto;
+  opacity: 0.5;
 `
 
 const Slider = () => {
   return (
     <Container>
-      <Arrow>
+      <Arrow direction="left">
         <ArrowLeftOutlined />
+      </Arrow>
+      <Arrow direction="right">
+        <ArrowRightOutlined />
       </Arrow>
     </Container>
   )
