74ebcaf595df07c4155faf90c46ab981b11d9350
leomunizq <leonardo.mdq@hotmail.com>
2022-06-15 19:54:00 +0200

    :feat: Newsletter section styles.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Categories from '../components/Categories'
 import Navbar from '../components/Navbar'
 import Slider from '../components/Slider'
 import Products from '../components/Products'
+import Newsletter from '../components/Newsletter'
 
 function Home() {
   return (
@@ -13,6 +14,7 @@ function Home() {
       <Slider />
       <Categories />
       <Products />
+      <Newsletter />
     </div>
   )
 }
